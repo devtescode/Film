bd65c08ceeda5256ca505be1535dbc45ca759c38
agboola teslim <teslimagboola09@gmail.com>
2023-11-14 17:19:39 +0100

    repush

--- src/component/Notfound.jsx
+++ src/component/Notfound.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
-import "./Notfound.css"
 import logo from '../assets/error.webp'
 import { useNavigate } from 'react-router-dom'
+import "./Notfound.css"
 const Notfound = () => {
     const navigate = useNavigate()
-    const homeBtn=()=>{
+    const homeBtn = () => {
         navigate("/home")
     }
     return (
         <>
-            <div className='container'>
+            <div className='smallscreen'>
                 <div className='mx-auto text-center'>
                     <img src={logo} className='styleerrimg' alt="" />
                     <p>This page isn't available click on the button below</p>
-                    <button onClick={homeBtn} className='btn btn-info'>Home</button>
+                </div>
+                <div className='justify-content-center arrowdivstyle p-3'>
+                    <div className='contdiv d-flex'>
+                        <h2>Home</h2>
+                    </div>
+                    <div className='mt-2'>
+                        <button onClick={homeBtn} className='btn btn-info fw-bold'>Home</button>
+                    </div>
                 </div>
             </div>
         </>
